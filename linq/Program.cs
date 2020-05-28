40ccdfa61ee34e42bff48f0a06700e5d1e0bda92
Heidi Sprouse <hsprouse91@gmail.com>
2020-05-28 15:30:08 -0500

    Working on LINQ exercises

---
+++ linq/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L"));
+
+            Console.WriteLine("Get foods that start with the letter 'L'");
+            Console.WriteLine("---------------------");
+            LFruits.ToList().ForEach(LFruit => Console.WriteLine(LFruit));
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 == 0);
+
+            Console.WriteLine();
+            Console.WriteLine("Get numbers that are multiples of 4 or 6");
+            Console.WriteLine("---------------------");
+            fourSixMultiples.ToList().ForEach(num => Console.WriteLine(num));
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather",
+                "James",
+                "Xavier",
+                "Michelle",
+                "Brian",
+                "Nina",
+                "Kathleen",
+                "Sophia",
+                "Amir",
+                "Douglas",
+                "Zarley",
+                "Beatrice",
+                "Theodora",
+                "William",
+                "Svetlana",
+                "Charisse",
+                "Yolanda",
+                "Gregorio",
+                "Jean-Paul",
+                "Evangelina",
+                "Viktor",
+                "Jacqueline",
+                "Francisco",
+                "Tre"
+            };
+
+            IEnumerable<string> descendingNames = names.OrderByDescending(name => name);
+
+            Console.WriteLine();
+            Console.WriteLine("Get names in descending order");
+            Console.WriteLine("---------------------");
+            descendingNames.ToList().ForEach(name => Console.WriteLine(name));
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> sortNumbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            Console.WriteLine();
+            Console.WriteLine("Sort numbers in ascending order");
+            Console.WriteLine("---------------------");
+            IEnumerable<int> ascendingNumbers = sortNumbers.OrderBy(num => num);
+
+            ascendingNumbers.ToList().ForEach(num => Console.WriteLine(num));
+
+            // Output how many numbers are in this list
+            List<int> countNumbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            int numberCount = countNumbers.Count;
+
+            Console.WriteLine();
+            Console.WriteLine("Count how many numbers are in a list");
+            Console.WriteLine("---------------------");
+            Console.WriteLine(numberCount);
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29,
+                745.31,
+                21.76,
+                34.03,
+                4786.45,
+                879.45,
+                9442.85,
+                2454.63,
+                45.65
+            };
+
+            double moneyMade = purchases.Sum();
+
+            Console.WriteLine();
+            Console.WriteLine("How much money have we made?");
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"${moneyMade}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45,
+                9442.85,
+                2454.63,
+                45.65,
+                2340.29,
+                34.03,
+                4786.45,
+                745.31,
+                21.76
+            };
+
+            IEnumerable<double> sortedPrices = prices.OrderByDescending(num => num);
+            double mostExpensive = sortedPrices.FirstOrDefault();
+
+            Console.WriteLine();
+            Console.WriteLine("Most expensive product");
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"${mostExpensive}");
+
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66,
+                12,
+                8,
+                27,
+                82,
+                34,
+                7,
+                50,
+                19,
+                46,
+                81,
+                23,
+                30,
+                4,
+                68,
+                14
+            };
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Expected output is { 66, 12, 8, 27, 82, 34, 7, 50, 19, 46 } 
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+
+            IEnumerable<int> numberList = wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 != 0);
+
+            Console.WriteLine();
+            Console.WriteLine("Store each number until a perfect square is detected");
+            Console.WriteLine("---------------------");
+            numberList.ToList().ForEach(num => Console.WriteLine(num));
+
+        }
+    }
+}
